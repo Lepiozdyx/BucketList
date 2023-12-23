a405cebdb6dc99792d336cf935e018e0388e1cab
Alex Zhulai <Lepiozdyx@gmail.com>
2023-12-23 16:16:02 -0300

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Alex on 23.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
