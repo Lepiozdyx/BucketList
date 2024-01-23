ec7d75f1fd7edaf12a0af52c8f660c9b9192123d
Alex Zhulai <Lepiozdyx@gmail.com>
2024-01-23 16:30:18 -0300

    BucketList app main task

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -6,16 +6,51 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @State private var viewModel = ViewModel()
+    
+    let startPosition = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: -34.6037, longitude: -58.3816),
+            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+        )
+    )
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        if viewModel.isUnlocked {
+            MapReader { proxy in
+                Map(initialPosition: startPosition) {
+                    ForEach(viewModel.locations) { location in
+                        Annotation(location.name, coordinate: location.coordinate) {
+                            Image(systemName: "star.circle")
+                                .resizable()
+                                .foregroundStyle(.red)
+                                .frame(width: 40, height: 40)
+                                .background(.white)
+                                .clipShape(.circle)
+                                .onLongPressGesture {
+                                    viewModel.selectedPlace = location
+                                }
+                        }
+                    }
+                }
+                .onTapGesture { position in
+                    if let coordinate = proxy.convert(position, from: .local) {
+                        viewModel.addLocation(at: coordinate)
+                    }
+                }
+                .sheet(item: $viewModel.selectedPlace) { place in
+                    EditView(location: place) {
+                        viewModel.update(location: $0)
+                    }
+                }
+            }
+        } else {
+            Button("Unlock map", action: viewModel.authenticate)
+                .buttonStyle(.bordered)
         }
-        .padding()
     }
 }
 
